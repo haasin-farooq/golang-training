09f30030c0cab3f3fa8236cbc5fb7976c7bd07cf
Haasin Farooq <haasin.farooq@tintash.com>
2021-06-11 12:34:00 +0500

    add function

--- hello.go
+++ hello.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("Hello, Haasin!")
+// }
+
+// func main() {
+// 	rand.Seed(time.Now().UTC().UnixNano())
+// 	fmt.Println("My favorite number is", rand.Intn(10))
+// }
+
+// func main() {
+// 	fmt.Printf("Now you have %v problems.\n", math.Sqrt(7))
+// }
+
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Println("Hello, Haasin!")
+	fmt.Println(add(40, 22))
 }
