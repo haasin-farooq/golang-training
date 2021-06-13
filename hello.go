4708e4fe9b36bfbbb92da2cd7d4adcc272004994
Haasin Farooq <haasin.farooq@tintash.com>
2021-06-13 22:13:17 +0500

    pointers

--- hello.go
+++ hello.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 // func main() {
@@ -76,21 +75,34 @@ import (
 // 	return z
 // }
 
-func Sqrt(x float64) float64 {
-	z := 1.0
-	var t float64
-	fmt.Printf("x = %v\n", x)
-	for {
-		z, t = z - (z*z - x) / (2*z), z
-		fmt.Printf("z = %v, t = %v\n", z, t)
-		if math.Abs(z-t) < 1e-6 {
-			break
-		}
-	}
-	return z
-}
+// func Sqrt(x float64) float64 {
+// 	z := 1.0
+// 	var t float64
+// 	fmt.Printf("x = %v\n", x)
+// 	for {
+// 		z, t = z - (z*z - x) / (2*z), z
+// 		fmt.Printf("z = %v, t = %v\n", z, t)
+// 		if math.Abs(z-t) < 1e-6 {
+// 			break
+// 		}
+// 	}
+// 	return z
+// }
+
+// func main() {
+// 	fmt.Println(Sqrt(3))
+// 	fmt.Printf("Sqrt(x) = %v\n", math.Sqrt(3))
+// }
 
 func main() {
-	fmt.Println(Sqrt(3))
-	fmt.Printf("Sqrt(x) = %v\n", math.Sqrt(3))
+	i, j := 42, 2701
+
+	p := &i         // point to i
+	fmt.Println(*p) // read i through the pointer
+	*p = 21         // set i through the pointer
+	fmt.Println(i)  // see the new value of i
+
+	p = &j         // point to j
+	*p = *p / 37   // divide j through the pointer
+	fmt.Println(j) // see the new value of j
 }
