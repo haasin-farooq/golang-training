5cac019bcfcc636b5a7d93f467ad35877f199501
Haasin Farooq <haasin.farooq@tintash.com>
2021-06-15 15:58:20 +0500

    JSON encoding and decoding

--- hello.go
+++ hello.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"math"
+	"os"
 )
 
 // func main() {
@@ -271,29 +272,98 @@ import (
 // 	}
 // }
 
-type ErrNegativeSqrt float64
+// type ErrNegativeSqrt float64
 
-func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+// func (e ErrNegativeSqrt) Error() string {
+// 	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+// }
+
+// func Sqrt(x float64) (float64, error) {
+// 	if x < 0 {
+// 		return 0, ErrNegativeSqrt(x)
+// 	}
+
+// 	z := 1.0
+// 	var t float64
+// 	for {
+//  		z, t = z - (z*z - x) / (2*z), z
+//  		if math.Abs(z-t) < 1e-6 {
+//  			break
+//  		}
+//  	}
+//  	return z, nil
+// }
+
+// func main() {
+// 	fmt.Println(Sqrt(2))
+// 	fmt.Println(Sqrt(-2))
+// }
+type response1 struct {
+    Page   int
+    Fruits []string
 }
-
-func Sqrt(x float64) (float64, error) {
-	if x < 0 {
-		return 0, ErrNegativeSqrt(x)
-	}
-	
-	z := 1.0
-	var t float64
-	for {
- 		z, t = z - (z*z - x) / (2*z), z
- 		if math.Abs(z-t) < 1e-6 {
- 			break
- 		}
- 	}
- 	return z, nil
+type response2 struct {
+    Page   int      `json:"page"`
+    Fruits []string `json:"fruits"`
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(-2))
+    bolB, _ := json.Marshal(true)
+    fmt.Println(string(bolB))
+
+    intB, _ := json.Marshal(1)
+    fmt.Println(string(intB))
+
+    fltB, _ := json.Marshal(2.34)
+    fmt.Println(string(fltB))
+
+    strB, _ := json.Marshal("gopher")
+    fmt.Println(string(strB))
+
+    slcD := []string{"apple", "peach", "pear"}
+    slcB, _ := json.Marshal(slcD)
+    fmt.Println(string(slcB))
+
+    mapD := map[string]int{"apple": 5, "lettuce": 7}
+    mapB, _ := json.Marshal(mapD)
+    fmt.Println(string(mapB))
+
+    res1D := &response1{
+        Page:   1,
+        Fruits: []string{"apple", "peach", "pear"}}
+    res1B, _ := json.Marshal(res1D)
+    fmt.Println(string(res1B))
+
+    res2D := &response2{
+        Page:   1,
+        Fruits: []string{"apple", "peach", "pear"}}
+    res2B, _ := json.Marshal(res2D)
+    fmt.Println(string(res2B))
+
+    byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
+	fmt.Println(string(byt))
+
+    var dat map[string]interface{}
+
+    if err := json.Unmarshal(byt, &dat); err != nil {
+        panic(err)
+    }
+    fmt.Println(dat)
+
+    num := dat["num"].(float64)
+    fmt.Println(num)
+
+    strs := dat["strs"].([]interface{})
+    str1 := strs[0].(string)
+    fmt.Println(str1)
+
+    str := `{"page": 1, "fruits": ["apple", "peach"]}`
+    res := response2{}
+    json.Unmarshal([]byte(str), &res)
+    fmt.Println(res)
+    fmt.Println(res.Fruits[0])
+
+    enc := json.NewEncoder(os.Stdout)
+    d := map[string]int{"apple": 5, "lettuce": 7}
+    enc.Encode(d)
 }
